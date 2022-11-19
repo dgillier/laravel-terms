acae9cfbc8fd7ef05a01d16d018328963ca4e2c0
denis <denis.gillier@gmail.com>
2022-11-19 16:58:35 +0100

    Add Spatie translation

--- src/Models/Term.php
+++ src/Models/Term.php
@@ -3,15 +3,21 @@
 namespace Nowendwell\LaravelTerms\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
 
 class Term extends Model
 {
+    use HasTranslations;
+
     /**
      * The table associated with the model.
      *
      * @var string
      */
     protected $table = "terms";
+
+    public $translatable = ['terms'];
 
     /**
      * The model's attributes.
